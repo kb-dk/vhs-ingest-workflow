a7eab2eb7c417d7f9243fbcffe4d2d7c04883dbc
ktc <ktc@statsbiblioteket.dk>
2012-09-04 14:16:23 +0200

    update workflow and doms ingester so we are getting values through

--- src/main/scripts/vhsingest.doms.ingester.sh
+++ src/main/scripts/vhsingest.doms.ingester.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+WD=$(pwd)
 cd $(dirname $(readlink -f $0))
 
 ENTITY=$1
@@ -14,6 +15,13 @@ NAME=`basename $0 .sh`
 source env.sh
 
 APPDIR="$VHSINGEST_COMPONENTS/${vhsingest.doms.ingester}"
+
+cd $WD
+#echo "lalalalalalala $YOUSEEMETADATA_LOCATION"
+echo "$@" > /tmp/foobar
+cat $YOUSEEMETADATA_LOCATION > /tmp/foobar2
+echo $(pwd) >> /tmp/foobar
+
 
 CMD="echo {\"domsPid\": \"uuid:9dabe130-f1d9-11e1-aff1-0800200c9a66\"}"
 #CMD="$JAVA_HOME/bin/java -cp $APPDIR/bin/*:$APPDIR/external-products/*:`dirname $CONFIGFILE` \
