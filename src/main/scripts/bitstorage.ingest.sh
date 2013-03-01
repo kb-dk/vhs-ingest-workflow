748c9b9de3c555807816c0ead400132e2a7f1258
ktc <ktc@statsbiblioteket.dk>
2013-03-01 10:59:25 +0100

    Fix bitstorage.ingest.sh script, to provide the right parameters to Bitrepository ingester

--- src/main/scripts/bitstorage.ingest.sh
+++ src/main/scripts/bitstorage.ingest.sh
@@ -3,14 +3,18 @@
 
 cd $(dirname $(readlink -f $0))
 
-ENTITY=$1
-LOCALFILE=$2
+ENTITY="$1"
+LOCALFILE=$(echo "$LOCALFILEURL" | sed -e 's/file:\/\///g')
+LOCALFILEURL="$2"
+REMOTEFILEID=$(echo "$LOCALFILEURL" | sed -e 's/file:\/\///g' | xargs basename)
+CHECKSUM=$(md5sum "$LOCALFILE" | cut -d' ' -f1)
+FILESIZE=$(stat -c%s "$LOCALFILE")
 
-NAME=`basename $0 .sh`
+NAME=$(basename $0 .sh)
 
 source env.sh
 
-CMD="$VHSINGEST_COMPONENTS/${vhsingest.bitrepository.ingester}/bin/${vhsingest.bitrepository.ingester}.sh $LOCALFILE $CONFIGFILE"
+CMD="$VHSINGEST_COMPONENTS/${vhsingest.bitrepository.ingester}/bin/${vhsingest.bitrepository.ingester}.sh $CONFIGFILE $LOCALFILEURL $REMOTEFILEID $CHECKSUM $FILESIZE"
 
 OUTPUT="`execute "$PWD" "$CMD" "$NAME" "$ENTITY"`"
 RETURNCODE=$?
