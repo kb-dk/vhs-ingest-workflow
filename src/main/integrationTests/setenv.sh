33498fbd052872f7a4167193e4f22d58667f6256
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-03-05 08:53:36 +0100

    Fixed a few filtering problems

--- src/main/integrationTests/setenv.sh
+++ src/main/integrationTests/setenv.sh
@@ -2,12 +2,15 @@ export TAVERNA_HOME="/home/yousee/taverna/taverna-workbench-2.4.0"
 export JAVA_HOME="/usr/java/jdk1.6.0_32"
 
 export VHSINGEST_HOME="$HOME/vhs-ingest-workflow/services/workflow"
-export VHSINGEST_CONFIG="${config}"
+export VHSINGEST_CONFIG="${install.config.dir}"
 export VHSINGEST_WORKFLOW_CONFIG="${workflow.config}"
-export VHSINGEST_LOGS="$HOME/${logs}"
-export VHSINGEST_LOCKS="$HOME/var/${locks}"
-export VHSINGEST_COMPONENTS="${home}/${components.dir}"
-export VHSINGEST_SCRIPTS="${home}/${script.dir}"
-export VHSINGEST_WORKFLOWS="${home}/${workflow.dir}"
-export VHSINGEST_DEPENDENCIES="${home}/${workflow.dependencies}"
-export VHSINGEST_BIN="${home}/${bin}"
+
+
+
+export VHSINGEST_LOGS="${install.home.dir}/${install.logs.dir}"
+export VHSINGEST_LOCKS="${install.home.dir}/${install.locks.dir}"
+export VHSINGEST_COMPONENTS="${install.home.dir}/${install.components.dir}"
+export VHSINGEST_SCRIPTS="${install.home.dir}/${install.script.dir}"
+export VHSINGEST_WORKFLOWS="${install.home.dir}/${workflow.dir}"
+export VHSINGEST_DEPENDENCIES="${install.home.dir}/${workflow.dependencies.dir}"
+export VHSINGEST_BIN="${install.home.dir}/${install.bin.dir}"
