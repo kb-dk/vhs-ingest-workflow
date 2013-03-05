8d4486066a3b29c2f4ce85149ded255c02f1ae8a
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-03-05 09:14:40 +0100

    Separation of doms ingesters

---
+++ src/main/scripts/doms.ingester.vhsfile.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+WD=$(pwd)
+cd $(dirname $(readlink -f $0))
+
+ENTITY=$1
+REMOTEURL=$2
+CHECKSUM=$3
+FFPROBEPROFILE_LOCATION=$4
+VHSMETADATA_LOCATION=$5
+USERNAME=$6
+PASSWORD=$7
+WSDL=$8
+
+
+NAME=$(basename $0 .sh)
+
+source env.sh
+
+APPDIR="$VHSINGEST_COMPONENTS/${doms.ingester}"
+
+cd $WD
+
+#CMD="echo {\"domsPid\": \"uuid:9dabe130-f1d9-11e1-aff1-0800200c9a66\"}"
+CMD="$JAVA_HOME/bin/java -cp $APPDIR/bin/*:$APPDIR/external-products/*:$(dirname $CONFIGFILE) \
+ dk.statsbiblioteket.doms.vhs.VHSIngesterCLI \
+ -filename $ENTITY \
+ -url $REMOTEURL \
+ -ffprobe $FFPROBEPROFILE_LOCATION \
+ -metadata $VHSMETADATA_LOCATION \
+ -config $CONFIGFILE \
+ -user $USERNAME \
+ -pass $PASSWORD \
+ -wsdl $WSDL"
+
+OUTPUT="`execute "$PWD" "$CMD" "$NAME" "$ENTITY"`"
+RETURNCODE=$?
+echo "$OUTPUT"
+exit "$RETURNCODE"
+
