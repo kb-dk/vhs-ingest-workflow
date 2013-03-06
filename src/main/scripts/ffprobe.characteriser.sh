2c6440d9e8de8399c343f94eb5b1ab9f8dadcffd
ktc <ktc@statsbiblioteket.dk>
2013-03-06 10:46:15 +0100

    make the workflow run until ffprobe validator..

--- src/main/scripts/ffprobe.characteriser.sh
+++ src/main/scripts/ffprobe.characteriser.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-
-pushd $(dirname $(readlink -f $0)) > /dev/null
+SCRIPT_PATH=$(dirname $(readlink -f $0))
 
 ENTITY=$1
 LOCALFILE=$2
 
 NAME=`basename $0 .sh`
 
-source env.sh
+source $SCRIPT_PATH/env.sh
 
 CMD="$VHSINGEST_COMPONENTS/${ffprobe.characteriser}/bin/ffprobeCharacterise.sh $LOCALFILE $CONFIGFILE"
 
 OUTPUT="`execute "$PWD" "$CMD" "$NAME" "$ENTITY"`"
 RETURNCODE=$?
 echo "$OUTPUT"
-
-popd > /dev/null
 exit "$RETURNCODE"
 
