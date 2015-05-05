40440ad4def54bf8bd796f36f9ab9b179ff3dc7c
Bolette <bam@statsbiblioteket.dk>
2015-05-05 12:41:13 +0200

    VHS-75 Implement packagemetadatafordoms component in Taverna workflow

--- src/main/integrationTests/vhsfileIntegrationTest.sh
+++ src/main/integrationTests/vhsfileIntegrationTest.sh
@@ -11,18 +11,14 @@ source "/opt/ffmpeg26/enable"
 which ffmpeg
 
 cd ..
-./bin/ingestVHS2File.sh "-inputvalue" "vhsfile" "/home/tvtape/testfiles/localhost/vhs2/localhost/dr1_digivid_1301643600-2011-04-01-09.40.00_1427911200-2015-04-01-20.00.00.ts" \
-"-inputvalue" "jsonfile" "/home/tvtape/testfiles/localhost/vhs2/localhost/dr1_digivid_1301643600-2011-04-01-09.40.00_1427911200-2015-04-01-20.00.00.ts.comments" \
+./bin/ingestVHSFile.sh "-inputvalue" "vhsfile" "/home/tvtape/testfiles/localhost/Colossus_20140923_1039.ts" \
+"-inputvalue" "vhslabel" "foobarbaz" \
+"-inputvalue" "vhsfilechecksum" "da974cfff522451ecb578f75efa9c5dc" \
+"-inputvalue" "starttime" "2006-06-29T22:05:56" \
+"-inputvalue" "stoptime" "2006-06-30T03:49:45" \
+"-inputvalue" "quality" "6" \
 "-inputvalue" "domsUser" "fedoraAdmin" \
 "-inputvalue" "domsPass" "fedoraAdminPass"
-#./bin/ingestVHSFile.sh "-inputvalue" "vhsfile" "/home/tvtape/testfiles/localhost/Colossus_20140923_1039.ts" \
-#"-inputvalue" "vhslabel" "foobarbaz" \
-#"-inputvalue" "vhsfilechecksum" "da974cfff522451ecb578f75efa9c5dc" \
-#"-inputvalue" "starttime" "2006-06-29T22:05:56" \
-#"-inputvalue" "stoptime" "2006-06-30T03:49:45" \
-#"-inputvalue" "quality" "6" \
-#"-inputvalue" "domsUser" "fedoraAdmin" \
-#"-inputvalue" "domsPass" "fedoraAdminPass"
 
 RETURNCODE=$?
 if [ "$RETURNCODE" -ne "0" ]; then
